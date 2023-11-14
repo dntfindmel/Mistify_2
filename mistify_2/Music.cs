a30878f65108aa7c8d36590ddff95de16d0acf42
Melyssa Moya <moyamelyssa@gmail.com>
2023-11-14 14:57:33 -0300

    Initial commit

---
+++ mistify_2/Music.cs
@@ -0,0 +1,23 @@
+﻿public class Music
+{
+    public string NomeMusica { get; set;}
+    public string Artista { get; set;}
+    public int Duracao { get; set;}
+    public bool Disponivel { get; set;}
+
+    public string Descricao => $"A música {NomeMusica} pertence ao artista {Artista}";
+    public void FichaTecnica()
+    {
+        Console.WriteLine($"\nNome: {NomeMusica}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("\nDisponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("\nAdquira no Plano Magic+");
+        }
+    }
+}
---
+++ mistify_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Music musica1 = new Music();
+musica1.NomeMusica = "Tear In My Heart";
+musica1.Artista = "Twenty One Pilots";
+musica1.Duracao = 308;
+musica1.Disponivel = true;
+
+
+Music musica2 = new Music();
+musica2.NomeMusica = "Back to black";
+musica2.Artista = "Amy Winehouse";
+musica2.Duracao = 400;
+musica2.Disponivel = false;
+
+musica1.FichaTecnica();
+musica2.FichaTecnica();
